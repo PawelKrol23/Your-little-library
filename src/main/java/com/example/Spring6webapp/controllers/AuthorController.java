cc61dadba294a924f86d280dd5a42e076d7e0e4b
PawelKrol23 <p.krol18a@gmail.com>
2023-08-07 12:27:26 +0200

    Added lombok constructors to controllers

--- src/main/java/com/example/Spring6webapp/controllers/AuthorController.java
+++ src/main/java/com/example/Spring6webapp/controllers/AuthorController.java
@@ -1,18 +1,17 @@
 package com.example.Spring6webapp.controllers;
 
 import com.example.Spring6webapp.services.AuthorService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class AuthorController {
 
     private final AuthorService authorService;
 
-    public AuthorController(AuthorService authorService) {
-        this.authorService = authorService;
-    }
     @RequestMapping("/authors")
     public String getAuthors(Model model) {
         model.addAttribute("authors", authorService.findAll());
