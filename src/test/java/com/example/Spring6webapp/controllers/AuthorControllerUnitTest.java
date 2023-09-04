1018634dea71ccde4ef8d26f5ed6c25c51a3c877
PawelKrol23 <p.krol18a@gmail.com>
2023-09-04 21:46:44 +0200

    Added tests for listing authors in AuthorController

---
+++ src/test/java/com/example/Spring6webapp/controllers/AuthorControllerUnitTest.java
@@ -0,0 +1,96 @@
+package com.example.Spring6webapp.controllers;
+
+import com.example.Spring6webapp.models.author.Author;
+import com.example.Spring6webapp.models.author.Nationality;
+import com.example.Spring6webapp.services.AuthorService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Collections;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(AuthorController.class)
+class AuthorControllerUnitTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    AuthorService service;
+
+    Author getTestAuthor() {
+        return Author.builder()
+                .firstName("test")
+                .lastName("test")
+                .nationality(Nationality.UNITED_KINGDOM)
+                .build();
+    }
+
+    @Test
+    void listAuthors_should_returnAuthorListView_when_authorPageIsNotEmpty() throws Exception {
+        // given
+        final var page = new PageImpl<>(Collections.singletonList(getTestAuthor()));
+        given(service.getAuthorPage(eq(0))).willReturn(page);
+
+        // when & then
+        mockMvc.perform(get(AuthorController.AUTHORS_PATH))
+                .andExpect(status().isOk())
+                .andExpect(view().name("author/list"))
+                .andExpect(model().attributeExists("authors"));
+    }
+
+    @Test
+    void listAuthors_should_respondWith404_when_authorPageIsEmpty() throws Exception {
+        // given
+        final var page = new PageImpl<Author>(Collections.emptyList());
+        given(service.getAuthorPage(eq(0))).willReturn(page);
+
+        // when & then
+        mockMvc.perform(get(AuthorController.AUTHORS_PATH))
+                .andExpect(status().isNotFound());
+    }
+
+//    @Test
+//    void createNewAuthorForm() {
+//    }
+//
+//    @Test
+//    void createNewAuthor() {
+//    }
+//
+//    @Test
+//    void editAuthorForm() {
+//    }
+//
+//    @Test
+//    void updateAuthorById() {
+//    }
+//
+//    @Test
+//    void deleteAuthorById() {
+//    }
+//
+//    @Test
+//    void addBookToAuthorPage() {
+//    }
+//
+//    @Test
+//    void addBookToAuthor() {
+//    }
+//
+//    @Test
+//    void removeBookFromAuthorPage() {
+//    }
+//
+//    @Test
+//    void removeBookFromAuthor() {
+//    }
+}
