d9ed49ca1456629abb21426e03bf95a18ce047bc
PawelKrol23 <p.krol18a@gmail.com>
2023-09-06 00:49:44 +0200

    Added tests for getting books in BookController

---
+++ src/test/java/com/example/Spring6webapp/controllers/BookControllerTest.java
@@ -0,0 +1,100 @@
+package com.example.Spring6webapp.controllers;
+
+import com.example.Spring6webapp.models.book.Book;
+import com.example.Spring6webapp.models.book.Genre;
+import com.example.Spring6webapp.services.BookService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Collections;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(BookController.class)
+class BookControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    BookService service;
+
+    Book getTestBook() {
+        return Book.builder()
+                .title("test")
+                .publicationYear(2020)
+                .genre(Genre.FANTASY)
+                .build();
+    }
+
+    @Test
+    void getBooks_should_returnBookListView_when_bookPageIsNotEmpty() throws Exception {
+        // given
+        final var page = new PageImpl<>(Collections.singletonList(getTestBook()));
+        given(service.getBookPage(eq(0))).willReturn(page);
+
+        // when & then
+        mockMvc.perform(get(BookController.BOOKS_PATH))
+                .andExpect(status().isOk())
+                .andExpect(view().name("book/list"))
+                .andExpect(model().attributeExists("books"));
+    }
+
+    @Test
+    void getBooks_should_respondWith404_when_bookPageIsEmpty() throws Exception {
+        // given
+        final var page = new PageImpl<Book>(Collections.emptyList());
+        given(service.getBookPage(eq(0))).willReturn(page);
+
+        // when & then
+        mockMvc.perform(get(BookController.BOOKS_PATH))
+                .andExpect(status().isNotFound());
+    }
+
+//    @Test
+//    void getSingleBook() {
+//    }
+//
+//    @Test
+//    void createNewBookForm() {
+//    }
+//
+//    @Test
+//    void createNewBook() {
+//    }
+//
+//    @Test
+//    void editBookForm() {
+//    }
+//
+//    @Test
+//    void updateBookById() {
+//    }
+//
+//    @Test
+//    void deleteBookById() {
+//    }
+//
+//    @Test
+//    void addAuthorToBookPage() {
+//    }
+//
+//    @Test
+//    void addBookToAuthor() {
+//    }
+//
+//    @Test
+//    void removeAuthorFromBookPage() {
+//    }
+//
+//    @Test
+//    void removeBookFromAuthor() {
+//    }
+}
